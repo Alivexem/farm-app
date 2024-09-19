d9ca969ab7c5718d10cab32174770d8fdf5ab0a0
alivexem <athkinstestimony@gmail.com>
2024-09-19 11:07:41 +0100

    more

--- app/genhead.tsx
+++ app/genhead.tsx
@@ -1,17 +1,18 @@
 import React from 'react'
 import Link from 'next/link';
-import Image from 'next/image';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
-  faMagnifyingGlass,
+  faTimes,
   faUserCircle,
   faBarsStaggered
 } from "@fortawesome/free-solid-svg-icons";
+
 interface GenheadProps {
   toggleMenu: () => void;
   clicked: boolean;
 }
-const Genhead: React.FC<GenheadProps> = ({ toggleMenu }) => {
+
+const Genhead: React.FC<GenheadProps> = ({ toggleMenu, clicked }) => {
   return (
     <div className='w-[full] h-[20vh] p-4 pt-0 ml-0 mr-0 pl-0 pr-0 mt-0'>
       <div className='bg-orange-500 flex justify-center items-center h-[70px] p-4 mb-3'>
@@ -21,24 +22,22 @@ const Genhead: React.FC<GenheadProps> = ({ toggleMenu }) => {
         <Image className='h-[90px] md:h-[14vh] lg:h-[18vh] w-[80px] md:w-[120px]' src='newfarme.png' alt='image' />
         <h1 className='text-[1.8rem]'>FARMDELY</h1>
         <div className='md:hidden'>
-        <Link href='/profile'><FontAwesomeIcon
-          className='ml-4 md:hidden'
-          icon={faUserCircle}
-          size='1x'
-        />
-        </Link>
+          <Link href='/profile'>
+            <FontAwesomeIcon
+              className='ml-4 md:hidden'
+              icon={faUserCircle}
+              size='1x'
+            />
+          </Link>
         </div>
         <div className='md:hidden'>
-        <FontAwesomeIcon
-
-          onClick={toggleMenu}
-          className='ml-3 md:hidden'
-          icon={faBarsStaggered}
-          size='1x'
-
-        />
+          <FontAwesomeIcon
+            onClick={toggleMenu}
+            className='ml-3 md:hidden'
+            icon={clicked ? faTimes : faBarsStaggered}
+            size='1x'
+          />
         </div>
-
       </div>
     </div>
   )
